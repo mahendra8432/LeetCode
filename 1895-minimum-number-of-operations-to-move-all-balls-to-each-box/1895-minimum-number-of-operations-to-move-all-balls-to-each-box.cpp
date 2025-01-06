63b1202568a1c77c692aafb61fa9e80851e75212
mahendra <153123561+mahendra8432@users.noreply.github.com>
2025-01-07 01:58:34 +0530

    Time: 49 ms (51.09%), Space: 12.9 MB (5.93%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int>v1;
-          vector<int>v2;
+         vector<int>v1;
+          //boxes ke andar jo jo 1 hai unke index v1 me bharo.
         for(int i=0;i<boxes.length();i++){
             if(boxes[i]=='1') v1.push_back(i);
         }
+        //ab har ek index i(0 to n) ko sare v1[i] se subtract karke(absolut) add karo 
+        //phir vo result v2[i] me store karo.
         int sizeofones=v1.size(),p=0;
         for(int i=0;i<boxes.length();i++){
                  p=0;
                 for(int j=0;j< sizeofones;j++){
                     p +=abs(v1[j]-i);
                 }
-                v2.push_back(p);
+                v1.push_back(p);
         } 
-        return v2;
+        // Remove the first N elements, and shift everything else down by N indices
+        v1.erase(v1.begin(), v1.begin() + sizeofones);
+        return v1;
     }
 };
