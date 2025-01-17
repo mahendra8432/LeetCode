549b2e3272cd6222c2103305f263317b2314da89
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-01-17 18:24:56 +0530

    Time: 3 ms (59.81%), Space: 264.1 MB (23.83%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        int x=0;
-        for(auto p:derived) x=x^p;
-        if(x==0) return true;
+        int q=0;
+        for(auto p:derived) q=q^p;
+        if(q==0) return true;
         else return false;
     }
 };
