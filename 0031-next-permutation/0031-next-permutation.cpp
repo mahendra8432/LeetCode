d368d70b3826207e1240786837892407f606f3d7
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-01-30 02:05:19 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.62%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,29 +1,6 @@
-#include <bits/stdc++.h>
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size(),p,q;
-        bool flag=true;
-        for(int i=n-1;i>0 && flag;i--){
-            if(nums[i]>nums[i-1]){
-                p=i-1;
-                int firstmax=INT_MAX;
-                sort(nums.begin()+p+1,nums.end());
-                while(i<n ) {
-                    if(nums[i]>nums[p]){
-                        swap(nums[i],nums[p]);
-                        flag=false;
-                        break;
-                    }
-                    
-                    //if(nums[p]>nums[i]) break;
-                    i++;
-                }
-               
-              
-                //v.begin() + l, v.begin() + r + 1
-            }
-        }
-        if(flag==true) reverse(nums.begin(),nums.end());
+        next_permutation(nums.begin(),nums.end());
     }
 };
