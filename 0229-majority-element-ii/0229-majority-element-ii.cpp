0568d685231318f8692fd5eea6505be47ecd0398
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-01-30 20:57:40 +0530

    Time: 0 ms (100%), Space: 19.8 MB (60.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        map<int,int>map;
-        vector<int>ans;
-        for(int i=0;i<n;i++) {
-            map[nums[i]]++;
-            if(map[nums[i]]>n/3) {
-                ans.push_back(nums[i]);
-                map[nums[i]]=-2*n;
-              }
-              
-            }
-            return ans;
+        int n = nums.size();
+        int count = 1, maxcount = 1, secondmax = 1, index, index2;
+        if(n==1) return nums;
+        else if(n==2 && nums[0]!=nums[1]) return nums;
+        else if(n==2) return {nums[0]};
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])  count++;
+            else{
+                if (count > maxcount) {
+                    secondmax = maxcount;
+                    maxcount = count;
+                    index = i-1;
+                } else if (count > secondmax) {
+                    secondmax = count;
+                    index2 = i-1;
+                }
+                 count=1;
+            } 
+            
+        }
+       int  i=n;
+              if (count > maxcount) {
+                    secondmax = maxcount;
+                    index2=index;
+                    maxcount = count;
+                    index = i-1;
+                } else if (count > secondmax) {
+                    secondmax = count;
+                    index2 = i-1;
+                }
+                 count=1;
+        if (maxcount > n / 3 && secondmax > n / 3)  return {nums[index], nums[index2]};
+        else if (maxcount > n / 3) return {nums[index]};
+        else return {};
     }
 };
