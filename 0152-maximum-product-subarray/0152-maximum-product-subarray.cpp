b53bbb28747b9f1fcfab544b49ae48b1efa9c879
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-02-01 04:17:06 +0530

    Time: 0 ms (100%), Space: 17.8 MB (57.19%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,52 +1,19 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        int prod=1,proin=1,profi=1,maxprod=1,indx1,ind,count=0,count2=0;
-        bool flag=true,flag2=true;
-        int maxi = *max_element(nums.begin(), nums.end());
-         if(maxi==0){
-            int p=0;
-            while(p<n-1 && flag2) {
-                if(nums[p]<0){
-                    if(nums[p+1]<0) flag2=false;
-                }
-                p++;
+        long long int prefix=1, suffix=1 ,ans = INT_MIN;
+        int n = nums.size();
+        for(int i=0;i<nums.size();i++){
+            if(prefix==0){
+                prefix=1;
             }
-            if(flag2==true) return 0;
+            if(suffix==0){
+                suffix=1;
+            }
+            prefix = prefix*nums[i];
+            suffix = suffix*nums[n-i-1];
+            ans = max(ans , max(prefix , suffix));
         }
-        for(int i=0;i<n;i++){
-            prod=1;proin=1;profi=1;flag=true,count=0;
-            while(i<n && nums[i]!=0){
-               if(nums[i]>=0) count2++;
-               if(flag) {  if(nums[i]<0) flag=false;
-                proin=proin*nums[i];
-                indx1=i;
-               }
-                prod=prod*nums[i];
-                if(nums[i]<0){ 
-                    count++;
-                    ind=i;
-                }
-             i++;
-            }
-            if(maxi<0  && count%2!=0) return max(prod/nums[0],prod/nums[n-1]);
-            if(count==1) {
-                 proin=proin/nums[indx1];
-                 prod=prod/nums[indx1];
-                 profi=prod/proin;
-                 prod=max(profi,proin);
-            }
-            else if(count%2!=0){
-                 while(ind<=i-1) {
-                    profi=profi*nums[ind];
-                     ind++;
-                    }
-                  prod=prod/max(proin,profi);
-            }
-            if(prod>maxprod) maxprod=prod;
-        }
-        return maxprod;
+        return ans;
     }
 };
