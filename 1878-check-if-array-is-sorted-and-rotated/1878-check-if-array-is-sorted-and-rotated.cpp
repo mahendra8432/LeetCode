fbfc202bdccdbb5374f53f5e7f06c9fd5dcd401c
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-02-02 23:46:54 +0530

    Time: 0 ms (100%), Space: 11.1 MB (58.89%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        // int n=nums.size(),minidx=0,min=INT_MAX;
-        // vector<int>v;
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]<min){
-        //         min=nums[i];
-        //         minidx=i;
-        //     }
-        // }
-        // for(int i=minidx;i<n;i++) v.push_back(nums[i]);
-        // for(int i=0;i<minidx;i++) v.push_back(nums[i]);
-        // for(int i=0;i<n-1;i++) if(v[i]>v[i+1]) return false;
-        // return true;
-        int count=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i+1]<nums[i]) count++;
+        int n=nums.size();
+        int min=INT_MAX;
+        bool flag=true;
+        for(int i=0;i<n-1;i++){
+            if(flag==false) if( nums[i]>nums[i+1]  || nums[i]>nums[0]) return false;
+            if(flag && nums[i]>nums[i+1]) flag=false;
         }
-        if(count>=2) return false;
-        else if(count==0) return true;
-        else if(nums[0]<nums[nums.size()-1]) return false;
+        if(!flag && nums[n-1]>nums[0]) return false;
         return true;
-
     }
 };
