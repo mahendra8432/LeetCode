8e1147b1530d8b33e774f44431144ec53d6f667c
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-02-05 20:14:14 +0530

    Time: 4 ms (33.49%), Space: 8.6 MB (41.79%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        bool flag=true;
         if(x<0) return false;
-        string s=to_string(x);
-        for(int i=0;i<s.length();i++){
-            if(s[i]!=s[s.length()-i-1]) flag=false;
+        // string s=to_string(x);
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]!=s[s.length()-i-1]) return false;
+        // }
+        // return true;
+        long long  r,n=(long long)x,sum=0;
+        while(n!=0){
+            r=n%10;
+            n=n/10;
+            sum=sum*10+r;
         }
-        if(flag==false) return false;
-        else  return true;
-        
+        if(sum==(long long)x) return true;
+        else return false;
     }
 };
