a8a123dd72614ee4e3134ac6a7b405d2b2a9547b
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-02-05 19:48:44 +0530

    Time: 0 ms (100%), Space: 26.2 MB (34.33%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n=nums.size();
-        if(n==1) return nums[0];
-        if(nums[0]!=nums[1]) return nums[0];
-        else if(nums[n-1]!=nums[n-2]) return nums[n-1];
         int low=0,high=n-1,mid;
-        while(high>=low){
+        while(high>low){
             mid=low+(high-low)/2;
-           if((high-low+1)%4==1){
-                if(nums[mid]==nums[mid-1]) high=mid;//single elment first part me hai;
-                else if(nums[mid]==nums[mid+1]) low=mid;
-                else return nums[mid];
-            }
-            else{
-                if(nums[mid]==nums[mid-1]) low=mid+1; //single elment first part me hai;
-                else if(nums[mid]==nums[mid+1]) high=mid-1;
-                else return nums[mid];
-            }
+            if(mid%2==1) mid--;
+            if(nums[mid]!=nums[mid+1]) high=mid;
+            else low=mid+2;
         }
-     return -1;
+        // ab low and high single elment ko target kar rahe hai;
+        return nums[low];
     }
 };
+
+/*ince every element in the sorted array appears exactly twice except for the single element, we know that if we take any element at an even index (0-indexed), the next element should be the same. Similarly, if we take any element at an odd index, the previous element should be the same. Therefore, we can use binary search to compare the middle element with its adjacent elements to determine which side of the array the single element is on.
+
+If the middle element is at an even index, then the single element must be on the right side of the array, since all the elements on the left side should come in pairs. Similarly, if the middle element is at an odd index, then the single element must be on the left side of the array.
+
+We can continue this process by dividing the search range in half each time, until we find the single element.*/
+
+
