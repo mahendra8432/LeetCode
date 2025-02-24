cdb911722abbdeb973afb62697d60807c30e8e6c
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-02-25 02:26:27 +0530

    Time: 0 ms (100%), Space: 28.1 MB (82.69%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,60 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-private:
-    // Function to search for a number in the postorder array
-    int search(vector<int>& postorder,int start,int end,int num)
-    {
-        for(int i=start;i<=end;i++)
-        {
-            if(postorder[i] == num)
-            {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-    // Variable to keep track of the index in the preorder array
-    int preindex = 0;
-    TreeNode* solve(vector<int>&preorder,vector<int>&postorder,int start,int end)
-    {
-        // Base case: empty subtree
-        if(start > end || preindex >= preorder.size())
-            return nullptr;
-
-        // Root value for the current subtree
-        int num = preorder[preindex];
-        preindex++;
-
-        TreeNode*root = new TreeNode(num);
-
-        // Leaf node or end of current subtree
-        if(preindex >= preorder.size() || start == end)
-            return root;
-
-        // The index of the next root value in the postorder array
-        int postindex = search(postorder,start,end,preorder[preindex]);
-        
-        //Recursive call for left and right subtree
-        root->left = solve(preorder,postorder,start,postindex);
-        // end - 1 because the root node is taken out
-        root->right = solve(preorder,postorder,postindex + 1,end - 1);
-
-        return root;
-    }
 public:
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
-        
-        return solve(preorder,postorder,0,preorder.size()-1);
+        stack<TreeNode*> nodes;
+        TreeNode* root = new TreeNode(preorder[0]);
+        nodes.push(root);
+
+        int preIndex = 1, postIndex = 0;
+
+        while (!nodes.empty()) {
+            TreeNode* current = nodes.top();
+
+            if (current->val == postorder[postIndex]) {
+                nodes.pop();
+                postIndex++;
+            } else {
+                TreeNode* newNode = new TreeNode(preorder[preIndex++]);
+                if (!current->left) {
+                    current->left = newNode;
+                } else {
+                    current->right = newNode;
+                }
+                nodes.push(newNode);
+            }
+        }
+        return root;
     }
 };
