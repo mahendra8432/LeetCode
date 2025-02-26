3cd230b90e2037aab204aec7112c3c8064ab2929
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-02-27 03:39:06 +0530

    Time: 2 ms (12.24%), Space: 8.7 MB (15.8%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n=s.length();
+        if(n!=goal.length()) return false;
+        for(int i=0;i<n;i++){
+            if(goal.find(s.substr(i,n-i+1))!=string::npos){
+                string p=s.substr(i,n-i+1);
+                 p+=s.substr(0,i-1+1);
+                 if(p==goal) return true;
+                 else return false;
+            }
+        }
+        //formality
+        return false;
+    }
+};
