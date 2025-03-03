f1c6834373f260a10f1e3fdb0167ef372e2bce9c
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-03-04 02:43:43 +0530

    Time: 0 ms (100%), Space: 14.7 MB (65.2%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        vector<vector<int>> ans ;
-        int n = nums1.size();
-        int m = nums2.size();
-        int i = 0 ; 
-        int j = 0 ; 
-        while(i < n || j < m){
-            if(nums1[i][0] == nums2[j][0]){
-                ans.push_back({nums1[i][0] , nums1[i][1] +nums2[j][1]});
-                i++,j++;
-            }
-            else if(nums1[i][0]<nums2[j][0]){
-                ans.push_back({nums1[i][0] , nums1[i][1]});
+        int n=nums1.size();
+        int m=nums2.size();
+
+        vector<vector<int>>ans;
+        int i=0,j=0;
+        while(i!=n && j!=m){
+            if(nums1[i][0]<=nums2[j][0]){
+                if(ans.size()>=1 && ans[ans.size()-1][0]==nums1[i][0]) ans[ans.size()-1][1]+=nums1[i][1];
+                else ans.push_back(nums1[i]);
                 i++;
             }
-            else{
-                ans.push_back({nums2[j][0] , nums2[j][1]});
+            else {
+                if(  ans.size()>=1 && ans[ans.size()-1][0]==nums2[j][0]) ans[ans.size()-1][1]+=nums2[j][1];
+                else ans.push_back(nums2[j]);
                 j++;
             }
-            while(i<n && j>=m){
-                ans.push_back({nums1[i][0] , nums1[i][1]});
+        }
+        if(i==n){
+            if(nums2[j][0]==ans[ans.size()-1][0]) {
+                ans[ans.size()-1][1]+=nums2[j][1];
+                j++;
+            }
+            ans.insert(ans.end(), nums2.begin()+j, nums2.end());
+        }
+        else if(j==m){
+                if(nums1[i][0]==ans[ans.size()-1][0]) {
+                ans[ans.size()-1][1]+=nums1[i][1];
                 i++;
-            }
-            while(j<m && i>=n){
-                ans.push_back({nums2[j][0] , nums2[j][1]});
-                j++;
-            }
-        } 
-        
+               }
+            ans.insert(ans.end(), nums1.begin()+i, nums1.end());
+        }
         return ans;
-        
+      
     }
 };
