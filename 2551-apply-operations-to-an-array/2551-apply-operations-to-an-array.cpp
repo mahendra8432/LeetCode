1412f4124b1edeb772c694a643321d369c317334
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-03-09 00:45:20 +0530

    Time: 0 ms (100%), Space: 13.1 MB (61.13%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        int n=nums.size(),i=0;
-        vector<int>ans;
-        for( i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]) {
                 nums[i]=2*nums[i];
                 nums[i+1]=0;
-                if(nums[i]!=0) ans.push_back(nums[i]);
                 i++;
             }
-            if(nums[i]!=0)ans.push_back(nums[i]);
         }
-        if(i!=n) if(nums[i]!=0)ans.push_back(nums[i]);
-        // arr.insert(arr.end(), n, x);
-        ans.insert(ans.end(), n-ans.size(), 0);
-        return ans;
-
+        int i=0,j=0;
+        bool flag=true;
+        for(int j=0;j<n;j++){
+            if(flag && nums[j]==0){ 
+                i=j; flag=false;
+            }
+            else if(nums[i]==0 && nums[j]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+            }   
+        }
+        return nums;
     }
 };
