15b7451e076b5c6904acbbcb5247c82c1c87c9ec
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-07-05 20:44:04 +0530

    Time: 3 ms (5.94%), Space: 10.7 MB (46.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int fun(int n,vector<int>&dp,vector<int>&nums){
-        int left,right=INT_MIN;
+        int pick,notpick;
         if(n==0) return nums[0];
-        if(n==1) return max(nums[0],nums[1]);
+        if(n<0) return 0;
         if(dp[n]!=-1) return dp[n];
-        left= fun(n-2,dp,nums) + nums[n];
-        right= (n>2)? fun(n-3,dp,nums):0 ;
-        right  =right + max(nums[n],nums[n-1]);
-        return dp[n]=max(left,right);
+        pick= fun(n-2,dp,nums) + nums[n];
+        notpick  =0 + fun(n-1,dp,nums);
+        return dp[n]=max(pick,notpick);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
