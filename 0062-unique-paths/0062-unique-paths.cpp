cff46e9fbd94c478f151764aa9b29c65743f1993
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-07-07 01:33:41 +0530

    Time: 2 ms (18.87%), Space: 9.3 MB (33.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-    // Method 3:-Space Optimization
+//Method 1:- Memorization
+    int  fun(int i,int j,vector<vector<int>>&dp){
+        int left,right;
+        if(i<0 || j<0) return 0;
+        if(i==0 || j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        left  = fun(i-1,j,dp);
+        right = fun(i,j-1,dp); 
+        return  dp[i][j]= left + right;
+    }
     int uniquePaths(int m, int n) {
-        if (m == 1 || n == 1) return 1;
-        vector<int>temp1(n,1),temp2(n,-1);
-        int prevleft=1,curri;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(i==0) {
-                    curri=prevleft;
-                    prevleft=curri;
-                    temp2[j]=curri;
-                }
-                else if(j==0) {
-                    curri=temp1[j];
-                    prevleft=curri;
-                    temp2[j]=curri;
-                }
-                else{
-                    curri=prevleft + temp1[j];
-                    prevleft=curri;
-                    temp2[j]=curri;
-                }
-            }
-            temp1=temp2;
-        }
-        return prevleft;
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return fun(m-1,n-1,dp);
     }
 };
-// value of each box is addition of value of box which is at left and at up.
-// 2 array lenge temp1  upar wale row ki sari vaues stoe karega. temp2 current row ki sari values store karega jo agale row me kaam ayengi.
