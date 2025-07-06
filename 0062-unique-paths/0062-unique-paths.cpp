7ec8a7fb0019f8ae4c4bb59f296dd8c88c72ef81
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-07-06 23:46:10 +0530

    Time: 0 ms (100%), Space: 9.5 MB (8.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-//Method 1:- Memorization
-    int  fun(int i,int j,vector<vector<int>>&dp){
-        int left,right;
-        if(i<0 || j<0) return 0;
-        if(i==0 || j==0) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        left  = fun(i-1,j,dp);
-        right = fun(i,j-1,dp); 
-        return  dp[i][j]= left + right;
-    }
+    // Method 2:-Tabulation
     int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) return 1;
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return fun(m-1,n-1,dp);
+        for(int i=0;i<max(m,n);i++){ 
+           if(i<n) dp[0][i]=1;
+           if(i<m) dp[i][0]=1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j]=dp[i][j-1] + dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
+// value of each box is addition of value of box which is at left and at up.
