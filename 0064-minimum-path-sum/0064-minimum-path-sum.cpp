3b2873d3e7c5c81952554cfb4a6f436c2f313f85
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-07-07 18:59:58 +0530

    Time: 0 ms (100%), Space: 15.6 MB (83.58%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-// Method 2:- Tabulation
+// Method 3:- Space Optimization
     int minPathSum(vector<vector<int>>& grid) {
-        int  left, right=0;
+        int  left, up;
+        int curri,prevleft=0;
         int m=grid.size(),n=grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        vector<int>tempc(n,-1),tempu(n,1);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                left=INT_MAX,right=INT_MAX;
-                if(i>0) left=dp[i-1][j];
-                if(j>0) right=dp[i][j-1];
-                if(i==0 && j==0) dp[0][0]=grid[0][0];
-                else dp[i][j]=grid[i][j] + min(left,right);
+                up=INT_MAX,left=INT_MAX;
+                if(i>0) up  =grid[i][j] + tempu[j];
+                if(j>0) left=grid[i][j] + prevleft;
+                curri=min(up,left);
+                prevleft=curri;
+                tempc[j]=curri;
+                if(i==0 && j==0) {
+                    curri=grid[0][0];
+                    prevleft=curri;
+                    tempc[j]=curri;
+                }
             }
+            tempu=tempc;
         }
-        return dp[m-1][n-1];
+        return prevleft;
     }
 };
 // same as unique path nut insted of returning 1 in base case return value of grid array
