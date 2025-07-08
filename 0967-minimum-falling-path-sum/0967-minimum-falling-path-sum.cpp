5d4e9d4452550668c4d408637c6ce053e7b23211
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-07-08 18:16:55 +0530

    Time: 0 ms (100%), Space: 14.5 MB (87.82%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-//Method 2:Tabulation ( fill dp from end to start.)
+//Method 3:Space optimization ( from end to start.)
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int m=matrix.size(), n=matrix[m-1].size();
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        int down,rightdown=INT_MAX,leftdown=INT_MAX,mini=INT_MAX;
+        vector<int>tempc(n,-1),tempb(n,-1);
+        int curri,down,rightdown=INT_MAX,leftdown=INT_MAX,mini=1e9;
         if(m==1) return *min_element(matrix[0].begin(), matrix[0].end());
-        for(int i=0;i<n;i++) {
-            dp[m-1][i]=matrix[m-1][i];
-        }
+        tempb=matrix[m-1];
         for(int i=m-2;i>=0;i--){
             for(int j=n-1;j>=0;j--){
-                rightdown=INT_MAX,leftdown=INT_MAX;
-                down=dp[i+1][j];
-                if(j<n-1) rightdown=dp[i+1][j+1];
-                if(j>0) leftdown=dp[i+1][j-1];
-                dp[i][j]= matrix[i][j] + min(down,min(rightdown,leftdown));
-                if(i==0) mini=min(mini,dp[0][j]);
+               rightdown=INT_MAX,leftdown=INT_MAX;
+               down=tempb[j];
+               if(j>0) leftdown=tempb[j-1];
+               if(j<n-1) rightdown=tempb[j+1];
+               curri=matrix[i][j] + min(down,min(leftdown,rightdown));
+               tempc[j]=curri;
+               if(i==0) mini=min(mini,curri);
             }
+            tempb=tempc;
         }
         return mini;
     }
