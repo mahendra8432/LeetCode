b73a9f0fa8c77a9eb7867d01d946526883f68ac7
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-07-08 16:43:10 +0530

    Time: 0 ms (100%), Space: 13.3 MB (31.24%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-// Methosd 1:- Memorization
-    int fun(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        int down,downright;
-        if(i==triangle.size()-1) return triangle[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        down=fun(i+1,j,triangle,dp);
-        downright=fun(i+1,j+1,triangle,dp);
-        return dp[i][j] = triangle[i][j] + min(down,downright);
-    }
+// method 2.2:- Tabulation from end to start because recursion is fron start to end.
     int minimumTotal(vector<vector<int>>& triangle) {
         int m=triangle.size(), n=triangle[m-1].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return fun(0,0,triangle,dp);
+        int down,rightdown=INT_MAX;
+        for(int i=0;i<n;i++) dp[m-1][i]=triangle[m-1][i];
+        for(int i=m-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                down=dp[i+1][j];
+                rightdown=dp[i+1][j+1];
+                dp[i][j]= triangle[i][j] + min(down,rightdown);
+            }
+        }
+        return dp[0][0];
     }
 };
-// here start point is fixed but no end point so 0--->1
-// remember ab apan start se end tak ja rahe hai.
-// only one base case is when we reach at last row so just return that element.
+// fill last row same as triangle's last row.
+// now fill second last row based on last row. similarly foe others
