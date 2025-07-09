1c709ae25d4443bdee8b363b7818c91ca0124c30
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-07-10 01:06:15 +0530

    Time: 151 ms (35.19%), Space: 17.4 MB (58.83%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-// without dp only using array and sorting.
-    bool fun(int i,int target,vector<int>& nums,vector<vector<int>>&dp){
-        if(nums[i]==target) return true;
-        if(i==0) return (nums[0]==target);
-        if(dp[i][target]!=-1) return dp[i][target];
-        bool nottake=fun(i-1,target,nums,dp);
-        bool take=false;
-        if(target>=nums[i]) take=fun(i-1,target-nums[i],nums,dp);
-        dp[i][target]=take || nottake;
-        return take || nottake;
-    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
@@ -18,7 +7,18 @@ public:
             sum+=nums[i];
         }
         if(sum%2!=0) return false;
-        vector<vector<int>>dp(n,vector<int>((sum/2)+1,-1));
-        return fun(n-1,sum/2,nums,dp);
+        int target=sum/2;
+        vector<vector<bool>>dp(n,vector<bool>(target+1,0));
+        for(int i=0;i<n;i++) dp[i][0]=true;
+        if(nums[0]<target+1)dp[0][nums[0]]=true;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=target;j++){
+                bool nottake=dp[i-1][j];
+                bool take=false;
+                if(j>=nums[i]) take=dp[i-1][j-nums[i]];
+                dp[i][j]=take || nottake;
+            }
+        }
+        return dp[n-1][target];
     }
 };
